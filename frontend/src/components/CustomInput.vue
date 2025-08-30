5f2e6f62af564e35d08dca18932d7820bcb5f3bf
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-08-30 06:37:48 +0200

    implemented POST call to frontend

--- frontend/src/components/CustomInput.vue
+++ frontend/src/components/CustomInput.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
-    defineProps({
-        type: {type: String, required: true, default: "text"},
-        placeholder: {type: String, required: false},
-        name: {type: String, required: false},
-        id: {type: String, required: false},
-    });
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+  type: { type: String, default: 'text' },
+  placeholder: String,
+  name: String,
+  id: String,
+  modelValue: { type: String, required: false }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const onInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).value);
+};
 </script>
 
 <template>
-    <input class="my-2 border-b-1 rounded-xs border-gray-300 p-1 focus:outline-1 "  :type=type :placeholder=placeholder :name=name :id=id>
+  <input
+    class="border-b-1 rounded-xs border-gray-300 p-1 focus:outline-1 my-2"
+    :type="type"
+    :placeholder="placeholder"
+    :name="name"
+    :id="id"
+    :value="modelValue"
+    @input="onInput"
+  >
 </template>
--- frontend/src/components/SignInUpForm.vue
+++ frontend/src/components/SignInUpForm.vue
@@ -4,27 +4,51 @@ import CustomButton from './CustomButton.vue';
 import { computed, ref } from 'vue';
 
 const props = defineProps({
-    type: {type:String, required: true}
+    type: { type: String, required: true }
 });
-if(!["signup","signin"].includes(props.type)){
+if (!["signup", "signin"].includes(props.type)) {
     throw new Error("Wrong type for SignInUpForm Component");
 }
 const isSignUp = computed(() => props.type === 'signup')
+
+const username = ref<string>();
+const email = ref<string>();
+const pwd1 = ref<string>();
+const pwd2 = ref<string>();
+
+
+
+
+const createUser = () => {
+    console.log(username)
+    fetch("/api/users", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: username.value,
+            email: email.value,
+            pwd: pwd1.value
+        })
+    }).then((res) => {
+        console.log(res.status);
+    });
+}
+
 </script>
 
 <template>
     <h1 class="text-2xl text-center">{{ isSignUp ? "Sign Up" : "Sign In" }}</h1>
     <form class="m-auto flex flex-col w-150 h-fit mt-5 p-2 justify-around">
         <label v-if="isSignUp" for="username">Username *</label>
-        <CustomInput v-if="isSignUp" type="text" name="username" id="username" />
+        <CustomInput v-if="isSignUp" type="text" name="username" id="username" v-model="username"/>
         <label for="email">Email *</label>
-        <CustomInput type="email" name="email" id="email" />
+        <CustomInput type="email" name="email" id="email" v-model="email"/>
         <label for="pwd1">Password *</label>
-        <CustomInput type="password" name="pwd1" id="pwd1" />
+        <CustomInput type="password" name="pwd1" id="pwd1" v-model="pwd1"/>
         <label v-if="isSignUp" for="pwd2">Password again *</label>
-        <CustomInput v-if="isSignUp" type="password" name="pwd2" id="pwd2" />
+        <CustomInput v-if="isSignUp" type="password" name="pwd2" id="pwd2" v-model="pwd2"/>
         <div class="flex justify-center mt-5">
-            <CustomButton>Submit</CustomButton>
+            <CustomButton @click="createUser()">Submit</CustomButton>
         </div>
     </form>
 </template>
