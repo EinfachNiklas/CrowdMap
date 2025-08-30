b1f5247132b98a55c64ce093316aea29d685b99e
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-08-30 05:46:30 +0200

    added .env files and vite-proxy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import userRouter from './routes/users';
 import swaggerRouter from './routes/swagger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const server = express();
 const port: number = Number(process.env.PORT ?? 6060);
