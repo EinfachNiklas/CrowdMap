b12ce5d4c5129b07cde1021bf9df857c0149ee97
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-09-19 03:26:40 +0200

    added toolbar with options

--- frontend/src/components/ToolBar.vue
+++ frontend/src/components/ToolBar.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import ToolbarOption from '@/components/ToolbarOption.vue';
+
+const handleAddEvent = ()=>{
+    console.log("ToDo: addEvent")
+};
 </script>
 
 <template>
-    <div class="flex justify-evenly bg-gray-800 opacity-90 w-150 h-15 rounded-[50px] absolute bottom-3 left-1/2 -translate-x-1/2 ">
+    <div
+        class="flex justify-evenly bg-gray-800 opacity-90 w-150 h-15 rounded-[50px] absolute bottom-3 left-1/2 -translate-x-1/2 ">
+        <ToolbarOption @clicking="handleAddEvent()" icon="ic:outline-add-location-alt" />
     </div>
 </template>
---
+++ frontend/src/components/ToolbarOption.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+defineProps({
+    icon: { type: String, required: true }
+});
+const emit = defineEmits<{ (e: 'clicking'): void }>();
+</script>
+
+<template>
+    <div class="flex flex-col justify-center">
+        <Icon @click.prevent="emit('clicking')" icon="ic:outline-add-location-alt"
+            class="text-white text-4xl cursor-pointer transition-all hover:scale-[1.15]" />
+    </div>
+</template>
