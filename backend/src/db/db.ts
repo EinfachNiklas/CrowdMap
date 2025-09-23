0d54c8305bfe6dab6370449d6aedc566e4abda5c
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-09-23 04:10:58 +0200

    added crowdEvent db tables

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 import fs from 'fs';
-const dbPath = path.join(__dirname,"../../data/app.db");
+const dbPath = path.join(__dirname, "../../data/app.db");
 fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 const db = new Database(dbPath);
 db.pragma('journal_mode = WAL');
@@ -24,6 +24,27 @@ db.exec(`
         expiresAt DATETIME NOT NULL,
         createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE
+    )
+    `);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS crowdEvents (
+        crowdEventId BLOB PRIMARY KEY NOT NULL,
+        title VARCHAR(64) NOT NULL,
+        lat DECIMAL(8,6) NOT NULL,
+        lon DECIMAL(9,6) NOT NULL,
+        createdBy INTEGER NOT NULL,
+        createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(createdBy) REFERENCES users(id) ON DELETE CASCADE
+    )
+    `);
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS crowdEventsVotings (
+        userid INTEGER NOT NULL,
+        crowdEventId BLOB NOT NULL,
+        FOREIGN KEY(userid) REFERENCES users(id) ON DELETE CASCADE
+        FOREIGN KEY(crowdEventId) REFERENCES crowdEvents(crowdEventId) ON DELETE CASCADE
     )
     `);
 
